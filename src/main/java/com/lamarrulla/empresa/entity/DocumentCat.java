8e062ca06814617d2694f937427bb1d510c5abb7
David Rincón <david.rincon@prototipo0.com>
2024-08-16 19:46:13 -0600

    cambios en catalogos

--- src/main/java/com/lamarrulla/empresa/entity/DocumentCat.java
+++ src/main/java/com/lamarrulla/empresa/entity/DocumentCat.java
@@ -1,9 +1,13 @@
 package com.lamarrulla.empresa.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
+@Data
+@Entity
 public class DocumentCat {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/lamarrulla/empresa/entity/TitleCat.java
+++ src/main/java/com/lamarrulla/empresa/entity/TitleCat.java
@@ -1,9 +1,13 @@
 package com.lamarrulla.empresa.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
+@Data
+@Entity
 public class TitleCat {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
